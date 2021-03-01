ab5259f9f047cae80b8e35266d0773fe4cef46b2
Luke Simmons <lukesmmns7@gmail.com>
2021-03-01 13:32:08 -0600

    Scope practice

---
+++ javascript.js
@@ -0,0 +1,16 @@
+
+let x = 1;
+
+function a() {
+  let y = 2;
+}
+
+function b() {
+  let z = 3;
+}
+
+function output(value) {
+  const para = document.createElement('p');
+  document.body.appendChild(para);
+  para.textContent = 'Value: ' + value;
+}
